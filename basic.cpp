aaf3f294a3d763b085a90e011d38bca370f8d642
Raghav0079 <raghav051006@gmail.com>
2026-06-15 09:46:57 +0530

    commit 10

--- basic.cpp
+++ basic.cpp
@@ -2,11 +2,7 @@
 using namespace std;
 
 int main(){
-    // range -> (-10^9 , 10^9)  
-
-    int numInt = INT_MAX ;
-    cout << numInt << endl;
-
-    long long numLong = 10000000000000;
+    string str = "cpp";
+    cout << str;
     return 0;
 }
