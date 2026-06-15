6be5b451122b4b9ec7f0b29d8956c044dd8ac892
Raghav0079 <raghav051006@gmail.com>
2026-06-15 09:58:44 +0530

    commit 14

--- basic.cpp
+++ basic.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 int main(){
-    string str;
-    getline(cin ,str);
-    cout << str;
-    
+    string str1 ,str2;
+
+    getline(cin ,str1);
+    getline(cin,str2);
+
+    cout << str1 << endl;
+    cout << str2;
 
     return 0;
 }
