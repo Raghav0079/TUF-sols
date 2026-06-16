635dc6b9d31ebbd7aff510e5d414e0c0b49a4c20
Raghav0079 <raghav051006@gmail.com>
2026-06-16 10:26:52 +0530

    commit 36

---
+++ Basic.java
@@ -0,0 +1,8 @@
+class Basic{
+    public static void main ( String[] args){
+        int num1 = -6;
+        int num2 = num1;
+        
+        System.out.println(num2);
+    }
+}
--- Basics.java
+++
@@ -1,8 +0,0 @@
-class Basics {
-    public static void main(String[] args) {
-        
-        char ch = 'abc' ;
-        System.out.println(ch);
-        
-    }
-}
