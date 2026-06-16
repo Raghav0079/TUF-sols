aa0b81f3b4c9c5a51088abe139460259f7b2d1e9
Raghav0079 <raghav051006@gmail.com>
2026-06-16 12:04:39 +0530

    commit 46

--- Basic.java
+++ Basic.java
@@ -1,9 +1,17 @@
 
 class Basic{
     public static void main ( String[] args){
-        int[][] arr = {{6,5} , {7,8,9} };
-        int [] zeroIndex = arr[0];
-        System.out.println(zeroIndex[0]);
+
+        int marks = 8;
+if (marks > 90) {
+    System.out.println("A");
+} else if (marks > 80) {
+    System.out.println("B");
+} else {
+    System.out.println("C");
+}
+        
+        
 
     }
 }
