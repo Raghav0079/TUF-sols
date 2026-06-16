615801208f0e892d3dd3bd3fd21fd9b9d919e34d
Raghav0079 <raghav051006@gmail.com>
2026-06-16 11:49:52 +0530

    commit 41

--- Basic.java
+++ Basic.java
@@ -1,9 +1,9 @@
+
 class Basic{
     public static void main ( String[] args){
-        String name = "rj";
-        String lastName = "jkl";
-        name += " " + lastName;
-        System.out.println(name);
-        
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.NextInt();
+        System.out.println(num);
+        scanner.close();
     }
 }
