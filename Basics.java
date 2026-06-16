cd8e3c72044db332402eb6057b349891a0eb1021
Raghav0079 <raghav051006@gmail.com>
2026-06-16 10:15:59 +0530

    commit 34

--- Basics.java
+++ Basics.java
@@ -1,7 +1,8 @@
 class Basics {
     public static void main(String[] args) {
-        System.out.println("hello!");
+        
+        int num = 100 ;
+        System.out.println(num);
         
     }
 }
-
