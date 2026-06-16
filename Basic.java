099069b7595bfbaf1aa69ebdb195d59c8be9ba64
Raghav0079 <raghav051006@gmail.com>
2026-06-16 12:02:28 +0530

    commit 45

--- Basic.java
+++ Basic.java
@@ -1,14 +1,9 @@
 
 class Basic{
     public static void main ( String[] args){
-        int[] arr = new int[5];
-        arr[0] = 1;
-        arr[1] = 6;
-        arr[2] = 7;
-        arr[3] = 9;
-        arr[4] = 1;
-        System.out.println(arr[3]);
-
+        int[][] arr = {{6,5} , {7,8,9} };
+        int [] zeroIndex = arr[0];
+        System.out.println(zeroIndex[0]);
 
     }
 }
