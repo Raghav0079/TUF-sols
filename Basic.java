281e7d9f3c71ed462c56c2d811fb2eda2261d8d4
Raghav0079 <raghav051006@gmail.com>
2026-06-16 10:47:50 +0530

    commit 39

--- Basic.java
+++ Basic.java
@@ -1,11 +1,7 @@
 class Basic{
     public static void main ( String[] args){
-        int num1 = 6;
-        int num2 = 7;
-
-        boolean result = num1 == num2;
-
-        System.out.println(result);
+        String name = "rj";
+        System.out.println(name);
         
     }
 }
