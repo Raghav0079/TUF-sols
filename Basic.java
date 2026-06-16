ee46f34bd6ccb52cf5c7dfe740b0b4dcef8afbaf
Raghav0079 <raghav051006@gmail.com>
2026-06-16 11:53:23 +0530

    commit 42

--- Basic.java
+++ Basic.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 
 class Basic{
     public static void main ( String[] args){
         Scanner scanner = new Scanner(System.in);
-        int num = scanner.NextInt();
-        System.out.println(num);
-        scanner.close();
+        
+        String firstName = scanner.next();
+        String lastName = scanner.next();
+        System.out.println(firstName + "" + lastName);
+
+        scanner.close(); 
     }
 }
