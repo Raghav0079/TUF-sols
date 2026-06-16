4b22856ee04e96282fe4e47353e1f1f3936346bf
Raghav0079 <raghav051006@gmail.com>
2026-06-16 10:52:17 +0530

    commit 40

--- Basic.java
+++ Basic.java
@@ -1,6 +1,8 @@
 class Basic{
     public static void main ( String[] args){
         String name = "rj";
+        String lastName = "jkl";
+        name += " " + lastName;
         System.out.println(name);
         
     }
