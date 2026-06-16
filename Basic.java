a443f7c9c570a544ad8a9c110a6cf66630a263de
Raghav0079 <raghav051006@gmail.com>
2026-06-16 12:11:00 +0530

    commit 48

--- Basic.java
+++ Basic.java
@@ -1,21 +1,11 @@
 
 class Basic{
     public static void main ( String[] args){
-        int day = 3;
-switch (day) {
-    case 1:
-        System.out.println("Monday");
-        break;
-    case 2:
-        System.out.println("Tuesday");
-        break;
 
-    
-
-    default:
-        System.out.println("Invalid");
+        for (int i = 0; i < 5; i++) {
+        System.out.println(i);
 }
 
+}
 
-    }
 }
