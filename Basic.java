6e3d12dc8e4e4b8ba7b3daa4cd06b1c7dd84cd60
Raghav0079 <raghav051006@gmail.com>
2026-06-16 10:27:17 +0530

    commit 37

--- Basic.java
+++ Basic.java
@@ -1,7 +1,7 @@
 class Basic{
     public static void main ( String[] args){
         int num1 = -6;
-        int num2 = num1;
+        int num2 = -num1;
         
         System.out.println(num2);
     }
