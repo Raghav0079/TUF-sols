1498c411567d7a8ba5499a218496a5f483d4269c
Raghav0079 <raghav051006@gmail.com>
2026-06-16 11:59:18 +0530

    commit 44

--- Basic.java
+++ Basic.java
@@ -1,9 +1,14 @@
 
 class Basic{
     public static void main ( String[] args){
-        int x = 10;
-        double y = x;
-        System.out.println(y);
-        
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 6;
+        arr[2] = 7;
+        arr[3] = 9;
+        arr[4] = 1;
+        System.out.println(arr[3]);
+
+
     }
 }
