3154aaad5da5a4b376833d8c0223edb6b394f7a3
Raghav0079 <raghav051006@gmail.com>
2026-06-16 12:13:14 +0530

    commit 50

--- Basic.java
+++ Basic.java
@@ -1,11 +1,11 @@
 
 class Basic{
     public static void main ( String[] args){
-        int i = 0;
-while (i < 5) {
-    System.out.println(i);
+int i = 0;
+do {
+    System.out.println(i); // Runs at least once
     i++;
-}
+} while (i < 5);
 }
 
 }
