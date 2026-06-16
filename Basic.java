618f766cc97ea4377b3c37a4bf482350e9a38655
Raghav0079 <raghav051006@gmail.com>
2026-06-16 12:41:56 +0530

    commit 54

--- Basic.java
+++ Basic.java
@@ -1,16 +1,18 @@
-import java.util.Scanner;
 
 class Basic{
     public static void main ( String[] args){
-        Scanner scanner = new Scanner(System.in);
-
-        int i =2;
-
-        while(i <= 50){
-            System.out.println(i);
-            i = i +2 ;
+        try{
+            int num1 = 10 ;
+            int num2 = 0 ;
+            int num3 = num1/num2;
+            System.out.println(num3);
         }
-        scanner.close();
+        catch (Exception ex){
+            System.out.println(ex);
+        }
+        finally{
+            System.out.println("done");
+        }
     }
 
 }
