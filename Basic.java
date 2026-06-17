588b5d9cb9174597fe520732d58eab1e9f260c65
Raghav0079 <raghav051006@gmail.com>
2026-06-17 10:19:59 +0530

    commit 59

--- Basic.java
+++ Basic.java
@@ -1,15 +1,21 @@
-import java.util.Scanner;
 
 class Test{
-    public static void printTest(){
-        System.out.println("hello");
+    int age ;
+    public void assignAge(int num){
+        age =num ;
     }
 }
 
 class Basic{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        Test test = new Test();
-        test.printTest();
+        Test test1 = new Test();
+        test1.assignAge(num:10);
+
+
+        Test test2 = new Test();
+        test2.assignAge(num:19);
+
+        System.out.println(test1.age);
+        System.out.println(test2.age);
     }
 }
