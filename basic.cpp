bdba0f9b4519e7a47c7dbc675878876961f29e50
Raghav0079 <raghav051006@gmail.com>
2026-06-17 10:10:40 +0530

    commit 57

--- basic.cpp
+++ basic.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main() {
     string str = "cppdsa";
     int length = str.size();
-    for ( int i =0 ; i<= length-1 ; i++){
-        cout << str[i]
+
+    for (int i = 0; i <= length - 1; i++) {
+        cout << str[i];
     }
-    
+
     return 0;
 }
