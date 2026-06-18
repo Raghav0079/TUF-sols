687ddc7c3f5f4beb5e2144a7f79b7ced5e85a314
Raghav0079 <raghav051006@gmail.com>
2026-06-18 11:22:21 +0530

    commit 72

---
+++ print_x_n.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    void printX(int X, int N) {
+        for (int i = 0; i < N; ++i) {
+            cout << X;
+
+            if (i < N - 1) {
+                cout << " ";
+            }
+        }
+
+        cout << endl;
+    }
+};
+
