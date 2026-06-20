f70d6007dfc14a95a3e334dcd42a2bee9cdfcb4f
Raghav0079 <raghav051006@gmail.com>
2026-06-20 22:43:31 +0530

    commit 98

--- pattern22.cpp
+++ pattern22.cpp
@@ -1,26 +1,24 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    int n ; 
-
-    for (int i = 0; i < 2 * n - 1; i++) {
-
-
-            for (int j = 0; j < 2 * n - 1; j++) {
-
-            
+class Solution {
+public:
+    void pattern22(int n) {
+        int size = 2 * n - 1;
+        
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                // Calculate distance from all 4 edges
                 int top = i;
+                int bottom = size - 1 - i;
                 int left = j;
-                int right = (2 * n - 2) - j;
-                int bottom = (2 * n - 2) - i;
-
-         
-                cout << (n - min(min(top, bottom), min(left, right)));
-                if (j < 2 * n - 2) cout << " ";
+                int right = size - 1 - j;
+                
+                // Value is n minus the minimum distance to any edge
+                int value = n - min({top, bottom, left, right});
+                
+                // Print the value followed by a trailing space
+                cout << value << " ";
             }
-
-            // Move to the next row
+            // Move to the next line after completing a row
             cout << endl;
-
-}
+        }
+    }
+};
