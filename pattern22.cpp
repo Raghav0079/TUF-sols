0feaf0762bcaea4aa99fba6c1170a7d75d0ecd71
Raghav0079 <raghav051006@gmail.com>
2026-06-20 22:37:47 +0530

    commit 97

---
+++ pattern22.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n ; 
+
+    for (int i = 0; i < 2 * n - 1; i++) {
+
+
+            for (int j = 0; j < 2 * n - 1; j++) {
+
+            
+                int top = i;
+                int left = j;
+                int right = (2 * n - 2) - j;
+                int bottom = (2 * n - 2) - i;
+
+         
+                cout << (n - min(min(top, bottom), min(left, right)));
+                if (j < 2 * n - 2) cout << " ";
+            }
+
+            // Move to the next row
+            cout << endl;
+
+}
