20136b712631a4d24b390a5b4a839d667259fb45
Raghav0079 <raghav051006@gmail.com>
2026-06-23 09:46:46 +0530

    commit 102

---
+++ STL2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void explainPair(){
+    pair<int,int>pr1= make_pair(2,8);
+    cout << pr1.first << " " << pr1.second << endl ;
+
+    pair<int,char>pr2 = make_pair(2,'b');
+    cout << pr2.first << " " << pr2.second << endl ;
+}
+
+int main(){
+    explainPair();
+    return 0;
+}
