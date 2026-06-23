4b8513c58d3f1621215afccd76a506c8a8544a34
Raghav0079 <raghav051006@gmail.com>
2026-06-23 10:48:14 +0530

    commit 118

--- STL7.cpp
+++ STL7.cpp
@@ -2,15 +2,17 @@
 using namespace std ;
 
 void explainPQ(){
-    priority_queue <int> pq :
+    priority_queue <int ,vector<int> , greater<int>> pq ;
     pq.push(5);
     pq.push(15);
     pq.push(51);
     pq.push(1);
 
-    cout << pq.top() << " ";
-    pq.pop();
-    cout << pq.top() << " ";
+    while(pq.empty() == false){
+        cout << pq.top() << " ";
+        pq.pop();
+        cout << endl ;
+    }
 }
 
 int main(){
