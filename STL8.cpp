742205bdac8ea0f934c11faed1ae9171c593c571
Raghav0079 <raghav051006@gmail.com>
2026-06-23 11:12:14 +0530

    commit 119

---
+++ STL8.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+void explainset(){
+    set<int> st;
+    st.insert(2);
+    st.insert(21);
+    st.insert(12);
+    st.insert(2);
+    st.insert(2);
+
+    for ( auto it : st){
+        cout << it << " ";
+    } 
+}
+
+int main(){
+    explainset();
+    return 0 ;
+}
