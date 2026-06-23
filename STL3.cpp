8f022576d4809360fb4302f19ddea4ea4f98b894
Raghav0079 <raghav051006@gmail.com>
2026-06-23 09:54:01 +0530

    commit 104

---
+++ STL3.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void explainPair(){
+    pair<int,int>pr1= make_pair(2,8);
+    cout << pr1.first << " " << pr1.second << endl ;
+
+    pair<int,char>pr2 = make_pair(2,'b');
+    cout << pr2.first << " " << pr2.second << endl ;
+
+    pair<pair<int , char> , int> pr3 = {{4,'h'} ,8};
+    cout << pr3.first.second<< endl ;
+}
+
+void explainVector(){
+    vector<int> vec ;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(0);
+    vec.emplace_back(10);
+
+    for( int i =0 ; i<vec.size();i++){
+        cout << vec[i] << " ";
+    }
+}
+
+int main(){
+    explainVector();
+    return 0;
+}
