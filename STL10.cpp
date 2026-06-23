12e7970b20da48090ac3624d731cfb1ea3e9cbf9
Raghav0079 <raghav051006@gmail.com>
2026-06-23 12:55:40 +0530

    commit 132

---
+++ STL10.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void explainsort(){
+    int arr[5] = {1,2,3,4,5};
+    sort(arr , arr+5);
+    for(int i =0 ; i<5 ; i++){
+        cout << arr[i] << " ";
+    }
+}
+
+
+int main(){
+    explainsort(); 
+    return 0;
+}
