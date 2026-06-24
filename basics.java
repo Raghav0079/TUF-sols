6a4d7d426e3aafc7286cf232ff0281c0f050f47c
Raghav0079 <raghav051006@gmail.com>
2026-06-24 10:26:19 +0530

    commit 142

--- basics.java
+++ basics.java
@@ -9,12 +9,20 @@ class Data{
         this.num = _num ;
         this.name = _name ;
     }
+    
 
 }
 
 public class basics{
     public static void main (String [] args){
         ArrayList<Data> aList = new ArrayList <> () ;
-        int arr[] = new int(100); 
+        aList.add(e:10);
+        aList.add(e:16);
+        aList.add(e:18);
+        aList.add(e:20);
+        System.out.println(aList);
+        System.out.println(aList.size());
+        System.out.println(aList.remove(index:2));
+        System.out.println(aList);
     }
 }
