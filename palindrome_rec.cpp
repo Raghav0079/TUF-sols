1b5db5605904b900d46bab99beea7376083c0dff
Raghav0079 <raghav051006@gmail.com>
2026-07-03 19:11:18 +0530

    commit 216

---
+++ palindrome_rec.cpp
@@ -0,0 +1,16 @@
+class Solution{	
+	public:		
+		bool palindromeCheck(string& s){
+            return isPalindrome(s,0,s.length() -1);
+        }
+    private:
+        bool isPalindrome(string& s, int left, int right){
+            if(left >= right){
+                return true;
+            }
+            if (s[left] != s[right]) {
+            return false;
+            }
+            return isPalindrome(s, left + 1, right - 1);
+        }
+};
