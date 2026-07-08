9a6850d95220633ce1b8e9fa481ec3f7dcd0eed3
Raghav0079 <raghav051006@gmail.com>
2026-07-08 14:49:35 +0530

    commit 247

--- Main3.java
+++ Main3.java
@@ -16,12 +16,24 @@ class ManualCar extends Car{
         System.out.println(x:"Hey I am manual");
     }
 }
+
+class AutomaticCar extends Car{
+    public void start(){
+        System.out.println(x:"Automatic car is starting");
+    }
+}
 public class Main3{
 
     public static void main(String[] args) {
         Car manualCar = new ManualCar();
         manualCar.start();
         manualCar.noise();
+
+        System.out.println();
+
+        Car automaticCar = new AutomaticCar();
+        automaticCar.start();
+        automaticCar.noise();
 
     }
 
