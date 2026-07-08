13d1c6d58cf1680d0fb4a5913559d1090244b537
Raghav0079 <raghav051006@gmail.com>
2026-07-08 16:28:31 +0530

    commit 257

--- Main4.java
+++ Main4.java
@@ -1,43 +1,53 @@
 import java.util.*;
 
-interface CarInterface{
-    static final int cnt =0;
+interface CarInterface {
+    static final int cnt = 0;
     public void start();
     public void numberOfGears();
 
-    default void airBags(){
+    default void airBags() {
         System.err.println("car has 5 airbags");
     }
-    static void printVehicleType(){
-        System.out.println(x:"its is a car");
+    
+    static void printVehicleType() {
+        System.out.println("it is a car");
     }
 }
 
-class ManualCar implements CarInterface{
-    public void start(){
+interface SunRoof {
+    public void sunRoof(); // Changed to camelCase to match standard Java conventions
+}
+
+class ManualCar implements CarInterface, SunRoof {
+    int doors = 4; // Defined 'doors' so the print statement works
+
+    public void start() {
         airBags();
-        System.out.println(x:"Manual car is starting..." + doors);
+        System.out.println("Manual car is starting..." + doors);
     }
-    public void numberOfGears{
-        System.out.println(x:"Manual cas has 6 gears");
+    
+    public void numberOfGears() { // Added missing ()
+        System.out.println("Manual car has 6 gears");
     }
+    
     @Override
-    public void airBags(){
-        System.out.println(x:"car has 6 airbags");
+    public void airBags() {
+        System.out.println("car has 6 airbags");
+    }
+
+    @Override
+    public void sunRoof() { // Implemented the missing SunRoof interface method
+        System.out.println("Manual car has a panoramic sunroof");
     }
 }
 
-interface SunRoof{
-    public void SunRoof();
-}
-
-public class Main4{
-    public static void main(String[] args){
-        CarInterface manualCar = new manualCar;
+public class Main4 {
+    public static void main(String[] args) {
+        // Changed reference type to ManualCar so it can access both interfaces' methods
+        ManualCar manualCar = new ManualCar(); 
+        
         manualCar.start();
-        manualCar.numberOfGears();;
-        manualcar.sunRoof();
-
-
+        manualCar.numberOfGears();
+        manualCar.sunRoof(); // Fixed typo casing 'manualcar' -> 'manualCar'
     }
 }
