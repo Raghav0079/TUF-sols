2cbf0990b0e28acc5358c18c52a1a7a647bc56d8
Raghav0079 <raghav051006@gmail.com>
2026-07-13 10:45:27 +0530

    commit 268

--- dll_to_array1.cpp
+++ dll_to_array1.cpp
@@ -1,14 +1,22 @@
-class Solution {
-public:
-    vector<int> toArray(Node* head) {
-        vector<int> result;
-        Node* curr = head; // Keep track of the current node
+/**
+ * Definition for doubly-linked list.
+ * class Node {
+ *     int val;
+ *     Node* prev;
+ *     Node* next;
+ *     Node() : val(0), next(nullptr), prev(nullptr) {}
+ *     Node(int x) : val(x), next(nullptr), prev(nullptr) {}
+ *     Node(int x, Node *prev, Node *next) : val(x), next(next), prev(prev) {}
+ * };
+ */
+vector<int> toArray(Node *head) {
+    vector<int> result;
+        Node* curr = head; 
         
         while (curr != nullptr) {
             result.push_back(curr->val);
-            curr = curr->next; // Move to the next node
+            curr = curr->next; 
         }
         
         return result;
-    }
-};
+}
