6816999f87dc64ef4693eccc431e52a6cb77c4b0
Raghav0079 <raghav051006@gmail.com>
2026-07-14 10:42:23 +0530

    commit 274

--- Main6.java
+++ Main6.java
@@ -1,59 +1,27 @@
 import java.util.*;
 
 
-
-class Student{
+class Passport{
     private String name ;
-    private List<Course> course;
-    Student(String name){
+    Passport (String name){
         this.name = name ;
-        courses = new ArrayList<>();
     }
-
-    public void addCourse(Course course){
-        courses.add(course);
-    }
-
-    public void displayAllStudents() {
-        for(int  i =0 ; i<students.size() ; i++){
-            System.out.println("Name" + students.get(i).getName());
-        }
-    }
- 
 }
 
-class Course{
+class Student{
     private String name;
-    private List<Course> students;
+    private Passport passport;
 
-    Course(String name){
-        this.name = name;
-        students = new ArrayList<> ();
-    }
-
-    public void addStudent(Student student){
-        students.add(student);
-    }
-
-    public void displayAllStudents() {
-        for(int  i =0 ; i<students.size() ; i++){
-            System.out.println("Name" + students.get(i).getName());
-        }
+    Student(String name , Passport passport ){
+        this.name = name ;
+        this.passport = passport ;
     }
 }
 
 
 public class Main6{
     public static void main(String[] args) {
-        Course maths = new Course(name:"Math");
-        Course cs = new Course(name:"CS");
-
-        Student abs = new Student(name : "abs");
-        Student abc = new Student(name : "abc");
-        Student abj = new Student(name : "abj");
-
-        abs.addCourse(math);
-        abs.addCourse(cs);
+        
 
     }
 }
