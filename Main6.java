d5b27bbf12519f8bb4d0f36dc2e74f04cb7ea8af
Raghav0079 <raghav051006@gmail.com>
2026-07-14 10:25:18 +0530

    commit 271

--- Main6.java
+++ Main6.java
@@ -1,37 +1,55 @@
 import java.util.*;
 
-class Passport{
-    private string passportNumber;
 
-    public Passport(String passportNumber) {
-        this.passportNumber = passportNumber;
-    }
-    public string getpassportNumber(){
-        return passportNumber;
-    }
-}
 
 class Student{
     private String name ;
-    private String passport;
+    private String id;
 
-    Student(String name , Passport passport){
+    Student(String name , String id){
         this.name = name ;
-        this.passport = passport;
+        this.id = id;
 
     }
-
-    public void displayDetails(){
-        System.out.println("student name :" + name);
-        System.out.println("Passport number" + passport.getpassportNumber());
+    public void getName(){
+        return name;
     }
+    public String getId(){
+        return id;
+    }
+
+    
 
 }
 
+class College{
+    private String name ;
+    private List<Student>  students;
+    College(String name){
+        this.name = name;
+        students = new ArrayList<>();
+    }
+    public static void addStudent(Student student) {
+        students.add(student);
+    }
+    public void printAllStudents(){
+        for(int i =0 ; i< students.size() ; i++){
+            Student student = students.get(i);
+            System.out.println("Name" + student.getName() + "id:" + student.getId());
+        }
+    }
+}
 public class Main6{
     public static void main(String[] args) {
-        Passport passport = new Passport(passportNumber : "123456789");
-        Student student = new Student("abs" , passport); 
-        
+        Student student1 = new Student(name:"aabs" , id :"1");
+        Student student2 = new Student(name:"abbs" , id :"2");
+
+        College college = new College(name : "JKL");
+        college.addStudent(student1);
+        college.addStudent(student2);
+
+        college.printAllStudents();
+
+
     }
 }
