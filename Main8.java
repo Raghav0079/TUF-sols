0cca4bd2b31c07b7afd9eb8475310f3a475d4f4f
Raghav0079 <raghav051006@gmail.com>
2026-07-16 21:31:49 +0530

    commit 293

--- Main8.java
+++ Main8.java
@@ -1,24 +1,28 @@
-import java.util.*;
 
 public class Main8{
     public static void main(String[] args){
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(e:10);
-        arr.add(e:15);
-
+        
         try {
-            int x = 2/0 ;
-            System.out.println(arr.get(index:5));
+            // trying to access a file 
+            // data connection 
+
+
+            // last line
             
-        } catch (ArithmeticException e) {
-            System.out.println(x:"Exception occurred at line 10" + e.getMessage());
-        }
-        catch(IndexOutOfBoundsException e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
+        }
+        finally{
+            // close this db connection 
 
+            try {
+                // close the db connection 
+            } catch (Exception e) {
+                System.out.println(x:"db connection was  not closed")
+            }
         }
 
-        System.out.println(x:"Hi");
+        
 
 
 
