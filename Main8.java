a2a06052cbbed74be3af3443b841c419c6b9c69c
Raghav0079 <raghav051006@gmail.com>
2026-07-16 21:22:15 +0530

    commit 290

--- Main8.java
+++ Main8.java
@@ -3,6 +3,18 @@ import java.util.*;
 public class Main8{
     public static void main(String[] args){
         ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(e:10);
+        arr.add(e:15);
+
+        try {
+            System.out.println(arr.get(index:5));
+            
+        } catch (Exception e) {
+            System.out.println(x:"Exception occurred at line 10" + e.getMessage());
+        }
+        System.out.println(x:"hi");
+
+
         
 
     }
