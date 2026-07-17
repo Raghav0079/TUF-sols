c632cb6c2a38e19cc9f354f4dd2ad39468da4c45
Raghav0079 <raghav051006@gmail.com>
2026-07-17 16:49:35 +0530

    commit 297

---
+++ Main9.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main9 {
+    public static void main(String[] args) {
+        
+        List<Integer> list = new ArrayList<>();
+        // 2. Fixed the method name and removed the IDE parameter hint
+        list.add(1); 
+        
+        // 3. Cleaned up the diamond operators
+        List<String> list1 = new ArrayList<>(); 
+        // 4. Removed the IDE parameter hint
+        list1.add("abs"); 
+    }
+}
