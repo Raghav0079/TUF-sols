c237f211ed6a3e8f06bf60680dfb4b1e396f179d
Raghav0079 <raghav051006@gmail.com>
2026-07-17 17:27:40 +0530

    commit 304

--- Main9.java
+++ Main9.java
@@ -2,46 +2,19 @@ import java.util.*;
 
 public class Main9 {
     
-    
     public static void main(String[] args) {
-        List ls = new ArrayList<>();
-        ls.add(e:10);
-        ls.add(e:10,9);
-        ls.add(e:"abs");
-        System.out.println(obj);
+        
     }
 }
 
-class GenericMethod {
-    public <T> void printArray(T[] arr){
-        for ( T val : arr){
-            System.out.print(val + " ");
-        }
-    }
-}
+class GenericMethod<T> {
+    T value;
 
-class Calculator<T extends Number> {
-    public double add(T a , T b){
-        return a.doubleValue() + b.doubleValue();
-    }
-}
-
-class ExampleGenerics<T> {
-    private List<T> list = new ArrayList<>();
-
-    public void add(T val) {
-        list.add(val);
+    public void setValue(T value){
+        this.value = value;
     }
 
-    public void removeLast() {
-        if (list.isEmpty()) {
-            System.out.println("list is empty");
-            return;
-        }
-        list.remove(list.size() - 1);
-    }
-
-    public T getElement(int index) {
-        return list.get(index);
+    public T getValue(){
+        return value;
     }
 }
