1900ac636f6c3d7d23c0093557b3e3362ffeeb9d
Raghav0079 <raghav051006@gmail.com>
2026-07-19 20:47:03 +0530

    commit 311

--- Main10.java
+++ Main10.java
@@ -3,23 +3,13 @@ import java.io.FileReader;
 
 public class Main10 {
     public static void main(String[] args) {
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader("C:\\Users\\Raghav\\OneDrive\\Desktop\\TUF-sols\\TUF-sols\\testing.txt"));
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Raghav\\OneDrive\\Desktop\\TUF-sols\\TUF-sols\\testing.txt"))) {
             String line;
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        } 
     }
 }
