f6385c48576335f8a4656a122a87e290c78a10d1
Raghav0079 <raghav051006@gmail.com>
2026-07-19 20:55:42 +0530

    commit 312

--- Main10.java
+++ Main10.java
@@ -1,15 +1,32 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+class Logger {
+    private String path;
+
+    Logger(String path) throws IOException {
+        File file = new File(path);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        this.path = path;
+    }
+
+    public void log(String message) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            bw.write(message);
+            bw.newLine();
+        } catch (Exception e) {
+            System.out.println("failed to log this message " + message);
+        }
+    }
+}
 
 public class Main10 {
-    public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Raghav\\OneDrive\\Desktop\\TUF-sols\\TUF-sols\\testing.txt"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        } 
+    public static void main(String[] args) throws IOException {
+        Logger logger = new Logger("C:\\Users\\Raghav\\OneDrive\\Desktop\\TUF-sols\\TUF-sols\\logfile.txt");
+        logger.log("log1");
     }
 }
