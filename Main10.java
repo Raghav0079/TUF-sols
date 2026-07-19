40e1d02e73f64b39baa56843642c41d9bb5689cd
Raghav0079 <raghav051006@gmail.com>
2026-07-19 20:31:21 +0530

    commit 310

--- Main10.java
+++ Main10.java
@@ -1,19 +1,21 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Main10 {
     public static void main(String[] args) {
-        BufferedWriter bw = null;
+        BufferedReader br = null;
         try {
-            bw = new BufferedWriter(new FileWriter("C:\\Users\\Raghav\\OneDrive\\Desktop\\TUF-sols\\TUF-sols\\testing.txt"));
-            bw.write("hey bro ");
-            bw.flush();
+            br = new BufferedReader(new FileReader("C:\\Users\\Raghav\\OneDrive\\Desktop\\TUF-sols\\TUF-sols\\testing.txt"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         } finally {
             try {
-                if (bw != null) {
-                    bw.close();
+                if (br != null) {
+                    br.close();
                 }
             } catch (Exception e) {
                 System.out.println(e.getMessage());
