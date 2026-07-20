385390fde84487018f73173d7909e4d2889524c3
Raghav0079 <raghav051006@gmail.com>
2026-07-20 20:47:03 +0530

    commit 313

---
+++ Main11.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+class Logger {
+    private String path;
+
+    Logger(String path) throws IOException {
+        File file = new File(path);
+
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        this.path = path;
+    }
+
+    public void log(String message) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            bw.write(message);
+            bw.newLine();
+        } catch (Exception e) {
+            System.out.println("failed to log this message: " + message);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Logger logger = new Logger("C:\\Users\\Raghav\\OneDrive\\Desktop\\TUF-sols\\TUF-sols\\testing.txt");
+    }
+}
