30645ed426ddb1cf420162d0777f7511246c1983
Raghav0079 <raghav051006@gmail.com>
2026-07-22 18:41:53 +0530

    commit 321

---
+++ rotate_left_k.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+
+    void reverseArray(vector<int>& nums, int start, int end) {
+
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++, end--;
+        }
+    }
+
+
+
+public:
+
+
+    void rotateArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k %n;
+
+        reverseArray(nums, 0, k - 1);
+        reverseArray(nums, k, n - 1);
+        reverseArray(nums, 0, n - 1);
+    }
+
+
+
+};
