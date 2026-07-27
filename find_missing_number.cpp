571a2f6178bfcf4164689ed3cfd1993996075208
Raghav0079 <raghav051006@gmail.com>
2026-07-27 14:43:10 +0530

    commit 329

---
+++ find_missing_number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int xor1=0 , xor2=0 ;
+
+        for(int i=0 ; i <nums.size() ; i++){
+            xor1 = xor1 ^ (i+1);
+            xor2 = xor2 ^ nums[i];
+        }
+        return (xor1 ^ xor2);
+        
+    }
+};
