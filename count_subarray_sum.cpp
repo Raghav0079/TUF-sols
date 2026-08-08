2e0edee31074f660b8181394d2de278579c35817
Raghav Mishra <raghav051006@gmail.com>
2026-08-08 14:02:49 +0530

    Update count_subarray_sum.cpp

--- count_subarray_sum.cpp
+++ count_subarray_sum.cpp
@@ -8,33 +8,27 @@ public:
     int subarraySum(vector<int> &nums, int k)
     {
         int n = nums.size(); 
-        // Number of subarrays
-        int count = 0;
+        unordered_map<int, int> prefixSumMap;
+        int currentPrefixSum = 0, subarrayCount = 0;
 
-        // starting index
-        for (int startIndex = 0; startIndex < n; startIndex++) { 
-            int currentSum = 0;
-            // ending index
-            for (int endIndex = startIndex; endIndex < n; endIndex++) { 
-                // calculate the sum of subarray [startIndex...endIndex]
-                // sum of [startIndex..endIndex-1] + nums[endIndex]
-                currentSum += nums[endIndex];
+       // Setting 0 in the map.
+        prefixSumMap[0] = 1; 
+        for (int i = 0; i < n; i++) {
+            // Add current element to the prefix sum:
+            currentPrefixSum += nums[i];
 
-                // Increase the count if currentSum == k:
-                if (currentSum == k)
-                    count++;
-            }
+            /*Calculate the value to 
+            remove (currentPrefixSum - k)*/
+            int sumToRemove = currentPrefixSum - k;
+
+            /* Add the number of subarrays
+             with the sum to be removed*/
+            subarrayCount += prefixSumMap[sumToRemove];
+
+            /* Update the count of current
+            prefix sum in the map*/
+            prefixSumMap[currentPrefixSum] += 1;
         }
-        return count;
+        return subarrayCount;
     }
 };
-
-int main() {
-    Solution solution;
-    vector<int> nums = {3, 1, 2, 4};
-    int k = 6;
-    // Function call to find the result
-    int count = solution.subarraySum(nums, k);
-    cout << "The number of subarrays is: " << count << "\n";
-    return 0;
-}
