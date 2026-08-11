3e8ab52bba25ebdc49b04241cbcdfa72b327c851
Raghav Mishra <raghav051006@gmail.com>
2026-08-11 21:44:42 +0530

    Create upper_bound.py

---
+++ upper_bound.py
@@ -0,0 +1,15 @@
+class Solution:
+    def upperBound(self, nums, x):
+        low,high = 0 ,len(nums) -1
+        ans = len(nums)
+
+        while low <= high :
+            mid=(low+high)//2
+            if nums[mid] > x:
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        return ans
+
+        
