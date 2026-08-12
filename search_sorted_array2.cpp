c88c2cda1f846a359073dd56436d39b542a243e2
Raghav Mishra <raghav051006@gmail.com>
2026-08-12 20:51:25 +0530

    Create search_sorted_array2.cpp

---
+++ search_sorted_array2.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    bool searchInARotatedSortedArrayII(vector<int>& arr, int target) {
+        int n = arr.size();
+        int low = 0, high = n - 1;
+         
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target) return true;
+
+            if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+                low = low + 1;
+                high = high - 1;
+                continue;
+            }
+
+
+            if (arr[low] <= arr[mid]) {
+
+                if (arr[low] <= target && target <= arr[mid]) {
+                    high = mid - 1;
+                } 
+                else {
+                    low = mid + 1;
+                }
+            } else {
+                if (arr[mid] <= target && target <= arr[high]) {
+                    low = mid + 1;
+                } 
+
+                else {
+                    high = mid - 1;
+                }
+            }
+        }
+        // If target is not found
+        return false;
+    }
+};
