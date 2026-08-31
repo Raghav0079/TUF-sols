c50c3108fb5953b1a55f98d6324b037d62c5e40a
Raghav0079 <raghav051006@gmail.com>
2026-08-31 20:29:24 +0530

    commit 391

---
+++ root_number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int floorSqrt(int n)  {
+        int ans = 0 ;
+
+        for(int i=1 ; i <=n ; i++){
+            long long val = 1LL*i*i;
+            if(val <= (long long)n){
+                ans = i;
+            }
+            else{
+                break;
+            }
+        }
+        return ans;
+      
+    }
+};
