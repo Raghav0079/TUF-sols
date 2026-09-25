fa22f80e188133831ca5f402889aa405cb5db724
Raghav0079 <raghav051006@gmail.com>
2026-09-25 09:09:29 +0530

    commit 422

--- majority_element1.cpp
+++ majority_element1.cpp
@@ -3,37 +3,47 @@ using namespace std;
 
 class Solution {
 public:
-
+    // Function to find the majority element in an array
     int majorityElement(vector<int>& nums) {
         
-
+        // Size of the given array
         int n = nums.size();
         
-        
-        int cnt = 0;
-        int el; 
-        
+        // Iterate through each element of the array
         for (int i = 0; i < n; i++) {
-            if (cnt == 0) {
-                cnt = 1;
-                el = nums[i];
-            } else if (el == nums[i]) {
-                cnt++;
-            } else {
-                cnt--;
+            
+            // Counter to count occurrences of nums[i]
+            int cnt = 0; 
+            
+            // Count the frequency of nums[i] in the array
+            for (int j = 0; j < n; j++) {
+                if (nums[j] == nums[i]) {
+                    cnt++;
+                }
+            }
+            
+            // Check if frequency of nums[i] is greater than n/2
+            if (cnt > (n / 2)) {
+                // Return the majority element
+                return nums[i]; 
             }
         }
-
-        int cnt1 = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == el) {
-                cnt1++;
-            }
-        }
-        if (cnt1 > (n / 2)) {
-            return el;
-        }
         
-        return -1;
+        // Return -1 if no majority element is found
+        return -1; 
     }
 };
+
+int main() {
+    vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
+    
+    // Create an instance of Solution class
+    Solution sol;
+ 
+    int ans = sol.majorityElement(arr);
+    
+    // Print the majority element found
+    cout << "The majority element is: " << ans << endl;
+    
+    return 0;
+}
